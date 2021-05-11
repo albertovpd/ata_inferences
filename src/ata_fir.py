2ed669606e655ce34332c37dca5cb2ba4d95e755
Vargas <a.vargas.pina@gmail.com>
2021-05-11 17:24:54 +0200

    fixing json to df & removing redundant columns

--- src/ata_fir.py
+++ src/ata_fir.py
@@ -10,16 +10,15 @@ from sklearn.linear_model import LinearRegression
 def ata_fir_function(data):
     '''Calculates the ata once the flight enters the fir'''
     
-    # add to the input dictionary distance from airports
-    df_coords= pd.DataFrame(data, index=[0]) #because of recycling from jupyter 
-    data["km_from_departure"]=haversine_distance(
-        df_coords, ['adep_latitude','adep_longitude']).values[0] 
+    # json to dataframe and adding distance from departure & arrival airports
+    json_df= pd.DataFrame(data, index=[0]) 
+    json_df["km_from_departure"]=haversine_distance(data, ['adep_latitude','adep_longitude']).values[0] 
 
     # transform str dates into numerical
     #df = str_to_int(data, [column_list_of_features])
 
     # match the input json with our template
-    df = redo_1hot_encoder(data) 
+    df = redo_1hot_encoder(json_df) 
 
     # bringing trained fit for our X
     pkl_filename = "output/trained_scaler.pkl"
